41a5a334ab8f9449709432d5ee4cefffc6ed7510
vlodo-o <vabramochkina@yandex.ru>
2022-12-16 01:16:39 +0300

    Реализация функций экрана настроек

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,44 @@
 package com.practicum.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val shareButton = findViewById<Button>(R.id.share)
+        shareButton.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.shared_url))
+                putExtra(Intent.EXTRA_TITLE, getString(R.string.shared_title))
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        val supportButton = findViewById<Button>(R.id.support)
+        supportButton.setOnClickListener {
+            val mailIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.mail_text))
+            }
+            startActivity(mailIntent)
+        }
+
+        val agreementButton = findViewById<Button>(R.id.agreement)
+        agreementButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.agreement_url)))
+            startActivity(browserIntent)
+        }
     }
 }
