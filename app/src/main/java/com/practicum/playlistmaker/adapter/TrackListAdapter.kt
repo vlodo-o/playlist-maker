6ac0771dbbafae924f89c2d37ed8f50b517308e5
vlodo-o <vabramochkina@yandex.ru>
2023-02-02 00:26:44 +0300

    Track cover in SearchActivity

--- app/src/main/java/com/practicum/playlistmaker/adapter/TrackListAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/adapter/TrackListAdapter.kt
@@ -3,8 +3,11 @@ package com.practicum.playlistmaker.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.model.Track
 
@@ -13,11 +16,19 @@ class TrackListAdapter(
 ) : RecyclerView.Adapter<TrackListAdapter.TrackViewHolder> () {
 
     class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val trackCoverImageView: ImageView = itemView.findViewById(R.id.track_cover)
         private val trackNameTextView: TextView = itemView.findViewById(R.id.track_name_textview)
         private val trackArtistTextView: TextView = itemView.findViewById(R.id.track_artist_textview)
         private val trackTimeTextView: TextView = itemView.findViewById(R.id.track_time_textview)
 
         fun bind(model: Track) {
+            val cornerRadius = itemView.resources.getDimensionPixelSize(R.dimen.track_cover_radius)
+            Glide.with(itemView)
+                .load(model.artworkUrl100)
+                .placeholder(R.drawable.track_placeholder)
+                .centerCrop()
+                .transform(RoundedCorners(cornerRadius))
+                .into(trackCoverImageView)
             trackNameTextView.text = model.trackName
             trackArtistTextView.text = model.artistName
             trackTimeTextView.text = model.trackTime
