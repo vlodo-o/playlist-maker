a83e604640042604253b53b342598a9459caa4c5
vlodo-o <vabramochkina@yandex.ru>
2023-03-04 23:18:14 +0300

    App theme in shared prefs

---
+++ app/src/main/java/com/practicum/playlistmaker/App.kt
@@ -0,0 +1,34 @@
+package com.practicum.playlistmaker
+
+import android.app.Application
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate
+
+const val DARK_THEME = "dark_theme"
+
+class App : Application() {
+    var darkTheme = false
+    private lateinit var sharedPrefs: SharedPreferences
+
+    override fun onCreate() {
+        super.onCreate()
+        sharedPrefs = getSharedPreferences(DARK_THEME, MODE_PRIVATE)
+        switchTheme(sharedPrefs.getBoolean(DARK_THEME, darkTheme))
+    }
+
+    fun switchTheme(darkThemeEnabled: Boolean) {
+        darkTheme = darkThemeEnabled
+        sharedPrefs.edit()
+            .putBoolean(DARK_THEME, darkThemeEnabled)
+            .apply()
+
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkThemeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
+
+}
